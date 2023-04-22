80484a58ea0df21bb2ff02614e152edd94c34b08
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-04-22 15:04:44 -0300

    edit css e js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Janela Popup //
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
